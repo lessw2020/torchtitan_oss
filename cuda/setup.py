debeacb6351dbacf5ba037f7767b2746d2f99da5
Less Wright <lessw@etrillium.com>
2024-05-20 20:51:39 -0700

    Create setup.py

---
+++ cuda/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import find_packages, setup
+from torch.utils.cpp_extension import CUDAExtension, BuildExtension
+
+
+setup(
+    name=f"titan_cuda",
+    description="Fused RMSNorm",
+    keywords="rmsnorm ",
+    version="1.5.20.2024",
+    url="https://github.com/lessw2020/4Bit_AdamW_Triton",
+    packages=find_packages(),
+    cmdclass={'build_ext': BuildExtension},
+    ext_modules=[
+        CUDAExtension(
+            name='rmsnorm_cuda',
+            sources=['rmsnorm_cuda.cpp', 'rmsnorm_cuda_kernel.cu'],
+            extra_compile_args={'cxx': [], 'nvcc': ['-lineinfo']}
+        ),
+
+
+
+    ],
+)
