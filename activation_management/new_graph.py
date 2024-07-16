09b647eca9c4c141a18bc1faf1c0db0c059e2a86
lessw2020 <lessw@etrillium.com>
2024-07-15 17:34:33 -0700

    start basic act management

--- activation_management/new_graph.py
+++ activation_management/new_graph.py
@@ -386,10 +386,18 @@ class manage_activations(saved_tensors_hooks):
     def __init__(self, pin_memory: bool = False, device_type: str = "cuda") -> None:
         device_module = getattr(torch, device_type, torch.cuda)
 
+        self.caching: bool = False # are we managing cpu cached memory blocks
         self.min_tensor_size_bytes = 1024 # we don't want to bother with small tensors
-        self.tracker = {} # track what saved/offloaded/compressed tensors, are where
-        self.mem_offload_cache = {} # cache of available memory for tensors
+        self.tracker = {} # tensor_id = (new_tensor, dtype, if_modified)  ---> track what saved/offloaded/compressed tensors, are where
+        self.mem_offload_cache = {} # cache of available memory blocks for tensors
         self.gb = 1024 * 1024 * 1024 # bytes in a gigabyte
+        self.ignore_types = [torch.complex64, torch.int64] # high precision and thus not good for quantization
+        self.is_first_forward = True
+        self.is_first_backward = True
+        # metrics
+        self.timing: bool = True
+        self.forward_start_time = 0
+        self.backward_start_time = 0
 
         # platform util functions
         def get_tensor_id()-> str:
@@ -401,18 +409,57 @@ class manage_activations(saved_tensors_hooks):
             num_bytes = self.get_num_bytes_tensor(x) 
             return tuple(num_bytes, x.size())
         
-        def get_num_bytes_tensor(self, x: torch.Tensor) -> int:
+        def get_num_bytes_tensor( x: torch.Tensor) -> int:
             # get the number of bytes in a tensor, for memory management purposes
-            return x.element_size() * x._base_storage().nbytes()
+            return tensor.element_size() * tensor.nelement() #x.element_size() * x._base_storage().nbytes()
         
-        def get_num_bytes_shape(self, x: torch.Tensor) -> int:
-            # get the number of bytes in a tensor's shape (e.g., for memory management purposes), assuming that we are storing it as a list of tensors
-            return 8 * len(x._base_storage().shape()) + sum([get_num_bytes_tensor(t) for t in x._base_storage().shape()])
+        def get_bytes_per_dimension(x: torch.Tensor)-> Tuple[int]:
+            # this might be too slow but is a way to provide a full byte signature for a tensor
+            # and used to match available memory sizes for caching 
+            # alternative = (total_bytes, tensor.shape) which does not account for strides
+            element_size = tensor.element_size()
+            shape = tensor.shape
+            stride = tensor.stride()
+            
+            bytes_per_dim = []
+            for dim, (size, stride_val) in enumerate(zip(shape, stride)):
+                bytes_in_dim = size * stride_val * element_size
+                bytes_per_dim.append(bytes_in_dim)
+    
+            return tuple(bytes_per_dim)
         
         # -------- core pack / unpack work --------
         def pack_tensor(activation: torch.Tensor) -> str:
             # activations are passed in during forward pass - from here we take over and return a unique id
+            if self.is_first_forward:
+                if self.timing:
+                    if self.backward_start_time:
+                    end_backward_time = time.perf_counter()
+                    print(f"***** backward pass took {(end_backward_time - backward_start_time):.3f} seconds")
+                    self.forward_start_time = time.perf_counter()
+                
+                print(f"total managed activations  {len(self.tracker)=}")
+                if not self.caching:
+                    self.tracking.clear()
+                
+                print(f"***** first forward")
+                self.is_first_forward = False
+                self.is_first_backward = True
+            
+            # query for basic tensor info
+            activation_dtype = activation.dtype
+            num_bytes = get_num_bytes_tensor(activation)
+            sizes = activation.size()
+            tensor_id = get_tensor_id()
 
+            # skipping complex types, small tensors, and tensors with unsupported dtypes
+            if num_bytes < min_tensor_size_bytes or (tensor_dtype in self.ignore_types):
+                print(f"skipping activation {input_tensor.shape=}, {input_tensor.dtype=}")
+                
+                gpu_clone = activation.clone().detach()
+                self.tracker[tensor_id] = (gpu_clone, activation.dtype, False)  #False = not modified
+                return tensor_id
+                
         def unpack_tensor(tensor_id: str) -> torch.Tensor:
             # backward pass - we are called with the tensor_id.  
             # We then use the tensor_id to retrieve the saved/offloaded/compressed tensor
