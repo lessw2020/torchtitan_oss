8cd6654f68f8cc6710bf04735e36f2dbb14c0372
lessw2020 <lessw@etrillium.com>
2024-07-15 18:03:17 -0700

    basic act management

--- activation_management/new_graph.py
+++ activation_management/new_graph.py
@@ -303,7 +303,7 @@ class saved_tensors_hooks:
         torch._C._autograd._pop_saved_tensors_default_hooks()
 
 
-class save_on_cpu_orig(saved_tensors_hooks):
+class save_on_cpu(saved_tensors_hooks):
     """Context manager under which tensors saved by the forward pass will be stored on cpu, then retrieved for backward.
 
     When performing operations within this context manager, intermediary
@@ -411,7 +411,7 @@ class manage_activations(saved_tensors_hooks):
         
         def get_num_bytes_tensor( x: torch.Tensor) -> int:
             # get the number of bytes in a tensor, for memory management purposes
-            return tensor.element_size() * tensor.nelement() #x.element_size() * x._base_storage().nbytes()
+            return x.element_size() * x.nelement() #x.element_size() * x._base_storage().nbytes()
         
         def get_bytes_per_dimension(x: torch.Tensor)-> Tuple[int]:
             # this might be too slow but is a way to provide a full byte signature for a tensor
@@ -434,13 +434,13 @@ class manage_activations(saved_tensors_hooks):
             if self.is_first_forward:
                 if self.timing:
                     if self.backward_start_time:
-                    end_backward_time = time.perf_counter()
-                    print(f"***** backward pass took {(end_backward_time - backward_start_time):.3f} seconds")
+                        end_backward_time = time.perf_counter()
+                        print(f"***** backward pass took {(end_backward_time - backward_start_time):.3f} seconds")
                     self.forward_start_time = time.perf_counter()
                 
                 print(f"total managed activations  {len(self.tracker)=}")
-                if not self.caching:
-                    self.tracking.clear()
+                #if not self.caching:
+                self.tracker.clear()
                 
                 print(f"***** first forward")
                 self.is_first_forward = False
@@ -453,17 +453,38 @@ class manage_activations(saved_tensors_hooks):
             tensor_id = get_tensor_id()
 
             # skipping complex types, small tensors, and tensors with unsupported dtypes
-            if num_bytes < min_tensor_size_bytes or (tensor_dtype in self.ignore_types):
+            if num_bytes < self.min_tensor_size_bytes or (activation_dtype in self.ignore_types):
                 print(f"skipping activation {input_tensor.shape=}, {input_tensor.dtype=}")
                 
                 gpu_clone = activation.clone().detach()
-                self.tracker[tensor_id] = (gpu_clone, activation.dtype, False)  #False = not modified
+                self.tracker[tensor_id] = (gpu_clone, activation.dtype, False)  # False = not modified
                 return tensor_id
-                
-        def unpack_tensor(tensor_id: str) -> torch.Tensor:
+            else:
+                # main activation management code
+                print(f"Storing activation {sizes}, {activation.dtype=} as {tensor_id}")
+                gpu_clone = activation.clone().detach()
+                self.tracker[tensor_id] = (gpu_clone, activation.dtype, True)  # True = (in future) modified
+                return tensor_id
+     
+        def unpack_tensor(unpack_tensor_id: str) -> torch.Tensor:
             # backward pass - we are called with the tensor_id.  
             # We then use the tensor_id to retrieve the saved/offloaded/compressed tensor
             # and return it in original state (or near original for quantized)
+            if self.is_first_backward:
+                self.is_first_backward = False
+                self.is_first_forward = True
+                if self.timing:
+                    end_forward_time = time.perf_counter()
+                    print(f"***** forward took {(end_forward_time - self.forward_start_time):.3f} seconds")
+                    print(f"***** first backward, managing {len(self.tracker)} tensors")
+                    self.backward_start_time = time.perf_counter()
+            
+            # retrieve the saved/offloaded/compressed tensor
+            assert unpack_tensor_id in self.tracker, f"untracked tensor, {unpack_tensor_id}"
+            gpu_tensor, dtype, modified = self.tracker[unpack_tensor_id]
+            # clear tensor from tracking
+            del self.tracker[unpack_tensor_id]
+            return gpu_tensor
 
         super().__init__(pack_tensor, unpack_tensor)
 
